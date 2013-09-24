297a51d1a50b5b852a4bcc533d804cde5fffcb21
tim a askew <askew_design@hotmail.com>
2013-09-24 07:59:39 -0500

    assignment 3 frame

---
+++ OOPAssignment3/src/oopassignment/DateInerface.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oopassignment;
+
+/**
+ *
+ * @author admin
+ */
+public interface DateInerface {
+    public int getYear();
+    public void setYear(int year);
+    public int getMonth();
+    public void setMonth(int month);
+    public int getDate();
+    public int getDate(int Date);
+    public int getDay();
+    public int getHours(int hours);
+    public int getMinutes();
+    public void setMinutes(int minutes);
+    public int getSeconds();
+    public long getTime();
+    public void setTime(long time);
+}
